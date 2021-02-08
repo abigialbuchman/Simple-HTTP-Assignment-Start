b475b46376864ae19042d904500595f3f368e675
Abigail Buchman <amb5759@rit.edu>
2021-02-07 19:47:04 -0500

    simple http project completed

---
+++ src/imageResponse.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+// pull in the file system
+const image = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+const getImage = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(image);
+  response.end();
+};
+module.exports.getImage = getImage;
